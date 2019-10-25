4b80fd71906d46ab351805de39a596eedc872705
Johan Meiring <johanmeiring@gmail.com>
2019-10-25 08:39:32 +0200

    Introduce some very sensible checks

--- functions/remove_host.fish
+++ functions/remove_host.fish
@@ -1,4 +1,14 @@
 function remove_host --description 'Remove host SSH key from ~/.ssh/known_hosts based on line number'
+    if test -z $argv
+        echo "You must provide a line number."
+        return 1
+    end
+
+    if not string match --quiet --regex --invert '\D' $argv
+        echo "Line number must be numeric."
+        return 1
+    end
+
     set sedcmd (which sed)
     if which gsed > /dev/null
         set sedcmd (which gsed)
